eba14de0ff321ce01d201b636a8755fe122ef124
Jetso Analin <jetyjetso@gmail.com>
2022-02-09 08:01:13 +0530

    Limit order dissabled

--- src/features/trade/Header.tsx
+++ src/features/trade/Header.tsx
@@ -37,7 +37,7 @@ const ExchangeHeader: FC<ExchangeHeaderProps> = ({ input, output, allowedSlippag
 
   return (
     <div className="flex items-center justify-between mb-4 space-x-3">
-      <div className="grid grid-cols-3 rounded bg-dark-800 h-[46px]">
+      <div className="grid grid-cols-2 rounded bg-dark-800 h-[46px]">
         <NavLink
           activeClassName="font-bold border rounded text-high-emphesis border-dark-800 bg-gradient-to-r from-opaque-blue to-opaque-pink hover:from-blue hover:to-pink"
           href={{
